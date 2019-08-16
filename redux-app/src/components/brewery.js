ecb6ff7cb332602552a7906d6118543863ed5195
Tracie T <traciecodes@gmail.com>
2019-08-15 19:47:22 -0500

    Final commit: added phone numbers

--- redux-app/src/components/brewery.js
+++ redux-app/src/components/brewery.js
@@ -6,6 +6,7 @@ const Brewery = props => {
     return(
         <div class= 'brewery-block'>
             <h2>{props.brewery.name}</h2>
+            <h3>Call: {props.brewery.phone} </h3>
             <a href={props.brewery.website_url} target='blank'>Website</a>
         </div>
     )
